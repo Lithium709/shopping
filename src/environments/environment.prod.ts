dc61b148233050643a96636e34136e58711c13c8
Oleksandr Lysyi <armiax@gmail.com>
2018-02-27 10:58:03 +0200

    new production settings

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://esarber.ukr-on.com',
-  authUrl:'http://arber.ukr-on.com',
+  apiUrl: 'http://search.arber.ua',
+  authUrl:'http://managepanel.arber.ua',
   //authUrl:'localhost:8080',
   deliveryApiKey: "e7175235694e986d34740ed2d9526420",
   deliveryUrl: "https://api.novaposhta.ua/v2.0/json/"
