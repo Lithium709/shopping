818c2271e46878ff78e0b3f96e9a2c0795ac8bfa
Oleksandr Lysyi <armiax@gmail.com>
2018-03-07 14:12:17 +0200

    real payment params

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,8 +5,8 @@ export const environment = {
   //authUrl:'localhost:8080',
   deliveryApiKey: "e7175235694e986d34740ed2d9526420",
   deliveryUrl: "https://api.novaposhta.ua/v2.0/json/",
-  merchantAccount: "test_merch_n1",
-  merchantSecretKey: "flk3409refn54t54t*FNJRET",
+  merchantAccount: "shop_arber_ua",
+  merchantSecretKey: "ff50fd07f4bfdaaff7d60b88abd94186dfc23726",
   merchantDomainName: "www.market.ua",
   prefix: "ARB"
 };
